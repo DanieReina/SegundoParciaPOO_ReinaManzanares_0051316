062d71c48e80811142789a80350ab10a075e76ae
unknown <00090123@uca.edu.sv>
2024-05-25 09:58:21 -0600

    ¨Parical2¨

---
+++ Parcial2.java
@@ -0,0 +1,8 @@
+/*En java crear una estructura que refleje la organizacion de la siguiente empresa:
+Una empresa clasifica sus departamentos en principales y secundarios, los departamentos principales responden directamente con el CEO, 
+mientras que los departamentos secundarios responden a alguno de los departamentos principales. Los empleados de la empresa pueden 
+estar asignados directamente bajo el CEO, pertenecientes a un departamento principal o, pertenecientes a un departamento secundario.
+
+Esta estructura de organizacion jerargica ayuda a la empresa a mantener una operacion eficiente y 
+permite  una clara asignacion de responsabilidades y facilita la gestion de proyectos. */
+
